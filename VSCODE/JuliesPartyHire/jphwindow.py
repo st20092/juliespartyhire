fd27e4f77fcbf93eedb9ed0f1202e9443761637d
st20092 <90332153+st20092@users.noreply.github.com>
2021-10-01 17:07:08 +1300

    Add files via upload

---
+++ VSCODE/JuliesPartyHire/jphwindow.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk() #Creates a window
+root.title ("Julie's Party Rental") #Sets the window a title
+root.iconbitmap('D:\VSCODE\JuliesPartyHire\img\logo.ico') #Sets the window icon
+root.geometry("912x513") #Sets the window dimensions
+root.configure(bg="#F8F8F9") #Configures the window
+
+
+root.mainloop()
